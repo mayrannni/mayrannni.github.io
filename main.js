8435d227255b6ea17c6f8edc2f8938c9bfedab36
Estrella Díaz <124010170+mayrannni@users.noreply.github.com>
2023-11-16 20:11:06 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+ cursor: "<span style='color: #754158;'>|</span>",
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('<span style="color: #754158;">Me gusta la tecnología</span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
